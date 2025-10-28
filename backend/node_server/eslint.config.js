7a3bbe46f1a8c0dff99468a2aef244f44c4ad2b1
Sam njoro <samnjorokibandi@gmail.com>
2025-10-28 09:34:47 +0300

    adding logger winston

--- backend/node_server/eslint.config.js
+++ backend/node_server/eslint.config.js
@@ -22,6 +22,32 @@ export default [
         rules: {
             indent: ['error', 2, { SwitchCase: 1 }],
             'linebreak-style': ['error', 'unix'],
-        }
-    }
-]
+            quotes: ['error', 'single'],
+            semi: ['error', 'always'],
+            'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+            'no-console': 'off',
+            'prefer-const': 'error',
+            'no-var': 'error',
+            'object-shorthand': 'error',
+            'prefer-arrow-callback': 'error',
+        },
+    },
+    {
+        files: ['tests/**/*.js'],
+        languageOptions: {
+          globals: {
+            describe: 'readonly',
+            it: 'readonly',
+            expect: 'readonly',
+            beforeEach: 'readonly',
+            afterEach: 'readonly',
+            beforeAll: 'readonly',
+            afterAll: 'readonly',
+            jest: 'readonly',
+          },
+        },
+      },
+      {
+        ignores: ['node_modules/**', 'coverage/**', 'logs/**', 'drizzle/**'],
+      },
+];
--- backend/node_server/server.js
+++ backend/node_server/server.js
@@ -2,11 +2,17 @@
 import cors from 'cors';
 import express from 'express';
 import http from 'http';
+import Logger from './config/logger.js';
 import 'dotenv.config';
 
 
 const app = express();
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('AuditAI Backend is running');
+    Logger.info('Root endpoint accessed');
+});
 
 const server = http.createServer(app);
 
