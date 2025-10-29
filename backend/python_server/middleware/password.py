c413c07397f8fee367b107971ed2d4a07a7a3106
Sam njoro <samnjorokibandi@gmail.com>
2025-10-29 22:20:09 +0300

    middleware setup for password authentication

---
+++ backend/python_server/middleware/password.py
@@ -0,0 +1,12 @@
+from passlib.context import CryptContext
+import jwt
+from jwt import PyJWTError
+
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+def get_password_hash(password: str) -> str:
+    return bcrypt_context.hash(password)
